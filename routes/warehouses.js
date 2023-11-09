3907970edeba35a2b77fc97943c62d191cba1354
Joshua George <joshua.vincent.george@gmail.com>
2023-11-09 13:44:54 -0800

    knex request for warehouse with id created

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const knex = require("knex")(require("../knexfile"));
 const router = express.Router();
 
 router.get("/", (req, res) => {
 	res.send("welcome to warehouses");
 });
 
+router.route("/:id").get((req, res) => {
+	knex("warehouses")
+		.where({ id: req.params.id })
+		.then((warehouse) => {
+			res.status(200).json(warehouse);
+		})
+		.catch(() => {
+			res.status(400).json({
+				message: `Error getting warehouse ${req.params.id}`,
+			});
+		});
+});
 module.exports = router;
