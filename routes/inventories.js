1e45ad8c3dfdd4ecbe2d726b3f4e7f378de57536
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 13:28:03 -0800

    WIP inventories knex request

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,12 +3,19 @@ const knex = require("knex")(require("../knexfile"));
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-	knex("inventories").then((inventory) => {
-		let newInv = inventory.map(
-			({ created_at, updated_at, ...remainingProps }) => remainingProps
-		);
-		res.send(newInv);
-	});
+	knex("inventories")
+		.join("warehouses", "warehouses.id", "inventories.warehouse_id")
+		.select("item_name", "warehouse_name")
+		.then((data) => {
+			res.send(data);
+		});
+
+	// .then((inventory) => {
+	// 	let newInv = inventory.map(
+	// 		({ created_at, updated_at, ...remainingProps }) => remainingProps
+	// 	);
+	// 	res.send(newInv);
+	// });
 });
 
 module.exports = router;
