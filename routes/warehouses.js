3ab0f6658a31bae9e95803940791411ff07bbb99
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 13:43:37 -0800

    removed knex import

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const knex = require("knex")(require("../knexfile"));
 const router = express.Router();
 const warehouseController = require("../controllers/warehouse-controller");
 
