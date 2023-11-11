4dcaea5d1e14f0be03852f26deaeb78dee10d55a
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 15:47:45 -0800

    GET list of inventory items functionality complete

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -1,19 +1,20 @@
 const knex = require("knex")(require("../knexfile"));
 
 const get = (req, res) => {
-	knex("inventories")
-		.join("warehouses", "warehouses.id", "inventories.warehouse_id")
-		.select("item_name", "warehouse_name")
+	knex("inventories as i")
+		.join("warehouses as w", "w.id", "i.warehouse_id")
+		.select(
+			"i.id",
+			"w.warehouse_name",
+			"i.item_name",
+			"i.description",
+			"i.category",
+			"i.status",
+			"i.quantity"
+		)
 		.then((data) => {
-			res.send(data);
+			res.status(200).send(data);
 		});
-
-	// .then((inventory) => {
-	// 	let newInv = inventory.map(
-	// 		({ created_at, updated_at, ...remainingProps }) => remainingProps
-	// 	);
-	// 	res.send(newInv);
-	// });
 };
 
 module.exports = {
