6bf6df862fdafa310098538d1fd3a4ab2142acff
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 22:57:18 -0800

    formatting

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,52 +1,56 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 const add = (req, res) => {
-    if (!req.body.contact_name || !req.body.contact_email) {
-      return res
-        .status(400)
-        .send("Please provide name and email for the user in the request");
-    }
+	if (!req.body.contact_name || !req.body.contact_email) {
+		return res
+			.status(400)
+			.send("Please provide name and email for the user in the request");
+	}
 
-    if (!req.body.warehouse_name || !req.body.address || !req.body.city || !req.body.country || !req.body.contact_position || !req.body.contact_phone) {
-        return res
-          .status(400)
-          .send("Please provide all the details");
-      }
-  
-    knex("warehouses")
-      .insert(req.body)
-      .then((result) => {
-        return knex("warehouses")
-          .where({ id: result[0] })
-      })
-      .then((addedWarehouse) => {
-        res.status(201).json(addedWarehouse);
-      })
-      .catch(() => {
-        res.status(500).json({ message: "Unable to create new user" });
-      })
-  };
+	if (
+		!req.body.warehouse_name ||
+		!req.body.address ||
+		!req.body.city ||
+		!req.body.country ||
+		!req.body.contact_position ||
+		!req.body.contact_phone
+	) {
+		return res.status(400).send("Please provide all the details");
+	}
 
-  const update = (req, res) => {
-    knex("warehouses")
-      .where({ id: req.params.id })
-      .update(req.body)
-      .then(() => {
-        return knex("warehouses").where({
-          id: req.params.id,
-        });
-      })
-      .then((updatedWarehouse) => {
-        res.json(updatedWarehouse[0]);
-      })
-      .catch(() => {
-        res
-          .status(500)
-          .json({ message: `Unable to update user with ID: ${req.params.id}` });
-      });
-  };
+	knex("warehouses")
+		.insert(req.body)
+		.then((result) => {
+			return knex("warehouses").where({ id: result[0] });
+		})
+		.then((addedWarehouse) => {
+			res.status(201).json(addedWarehouse);
+		})
+		.catch(() => {
+			res.status(500).json({ message: "Unable to create new user" });
+		});
+};
 
-  module.exports = {
-    add,
-    update
-  }
+const update = (req, res) => {
+	knex("warehouses")
+		.where({ id: req.params.id })
+		.update(req.body)
+		.then(() => {
+			return knex("warehouses").where({
+				id: req.params.id,
+			});
+		})
+		.then((updatedWarehouse) => {
+			res.json(updatedWarehouse[0]);
+		})
+		.catch(() => {
+			res
+				.status(500)
+				.json({ message: `Unable to update user with ID: ${req.params.id}` });
+		});
+};
+
+module.exports = {
+	add,
+	update,
+};
