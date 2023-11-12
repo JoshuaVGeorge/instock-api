ef4559671a857a9e0806550f599eafb04dd0f77e
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 16:01:44 -0800

    changed get to getAllItems

--- routes/inventories.js
+++ routes/inventories.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const inventoriesController = require("../controllers/inventories-controller");
 
-router.route("/").get(inventoriesController.get);
+router.route("/").get(inventoriesController.getAllItems);
 
 module.exports = router;
