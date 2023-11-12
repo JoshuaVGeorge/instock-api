b79950bde7c0d8e3da9da803b54009349d68fc45
Joshua George <joshua.vincent.george@gmail.com>
2023-11-11 16:20:24 -0800

    added data retrieval fro getItemByWarehouse

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -14,9 +14,30 @@ const getAllItems = (req, res) => {
 		)
 		.then((data) => {
 			res.status(200).send(data);
+		})
+		.catch((err) => {
+			res.status(500).send(`${err}`);
+		});
+};
+
+const getItemByWarehouse = (req, res) => {
+	knex("warehouses as w")
+		.join("inventories as i", "w.id", "i.warehouse_id")
+		.select(
+			"i.id",
+			"i.item_name",
+			"i.category",
+			"i.status",
+			"i.quantity",
+			"w.warehouse_name"
+		)
+		.where({ "w.id": req.params.id })
+		.then((data) => {
+			res.send(data);
 		});
 };
 
 module.exports = {
 	getAllItems,
+	getItemByWarehouse,
 };
